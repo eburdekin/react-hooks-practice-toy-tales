7f061685c6f7f7e73cbe35c17a17a60320766211
Eileen <eileenburdekin@proton.me>
2023-11-28 21:35:10 -0800

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
 import ToyContainer from "./ToyContainer";
+
+const toyAPI = 'http://localhost:3001/toys'
 
 function App() {
   const [showForm, setShowForm] = useState(false);
@@ -11,14 +13,59 @@ function App() {
     setShowForm((showForm) => !showForm);
   }
 
+  const [toys, setToys] = useState([])
+
+  useEffect(()=>{
+    fetch(toyAPI)
+    .then(r => r.json())
+    .then(setToys)
+  },[])
+
+  function handleAddToy(newToy) {
+    setToys([...toys, newToy])
+  }
+
+  function handleDelete(id) {
+    fetch(`${toyAPI}/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    })
+    setToys(toys.filter(toy => id !== toy.id))
+  }
+
+  function handleLikes(likedToy) {
+
+    const newLikes = likedToy.likes +1
+    const updatedLikes = {likes: newLikes}
+
+    fetch(`${toyAPI}/${likedToy.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(updatedLikes)
+    })
+    .then(r => r.json())
+    .then(
+    setToys(toys.map(toy => {
+      if (toy.id === likedToy.id) {
+        return {...toy, likes: newLikes }
+      }
+      else {return toy}
+    }))
+    )
+  }
+
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+      {showForm ? <ToyForm handleAddToy={handleAddToy}/> : null}
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer toys={toys} handleDelete={handleDelete} handleLikes={handleLikes}/>
     </>
   );
 }
--- src/components/ToyForm.js
+++ src/components/ToyForm.js
@@ -1,15 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
 
-function ToyForm() {
+const toyAPI = 'http://localhost:3001/toys'
+
+function ToyForm({handleAddToy}) {
+
+  const [name, setName] = useState('')
+  const [image, setImage] = useState('')
+
+  function handleName(e) {
+    setName(e.target.value)
+  }
+
+  function handleImage(e) {
+    setImage(e.target.value)
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newToy = {
+      name: name,
+      image: image,
+      likes: 0, 
+    }
+    fetch(toyAPI, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newToy)
+    })
+    .then(r => r.json())
+    .then(newToy => handleAddToy(newToy))
+  }
+
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form className="add-toy-form" onSubmit={handleSubmit}>
         <h3>Create a toy!</h3>
         <input
           type="text"
           name="name"
           placeholder="Enter a toy's name..."
           className="input-text"
+          value={name}
+          onChange={handleName}
         />
         <br />
         <input
@@ -17,6 +51,8 @@ function ToyForm() {
           name="image"
           placeholder="Enter a toy's image URL..."
           className="input-text"
+          value={image}
+          onChange={handleImage}
         />
         <br />
         <input
